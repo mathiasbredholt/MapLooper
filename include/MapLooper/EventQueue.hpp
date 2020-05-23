0b253eef7eb0bc1debf2b648a824157c29645411
mathiasbredholt <mathiasbredholt@gmail.com>
2020-05-23 15:52:19 -0400

    Updated license header to GPL

--- include/MapLooper/EventQueue.hpp
+++ include/MapLooper/EventQueue.hpp
@@ -1,25 +1,29 @@
 /*
-           __  
-  /\/\    / /  
- /    \  / /   
-/ /\/\ \/ /___ 
-\/    \/\____/ 
-MapLooper
-(c) Mathias Bredholt 2020
+ MapLooper - Embedded Live-Looping Tools for Digital Musical Instruments
+ Copyright (C) 2020 Mathias Bredholt
 
-*-0-*-0-*-0-*-0-*-0-*-0-*-0-*-0-*-0-*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-EventQueue.hpp
-A priority queue implementation
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
+
+#pragma once
 
 #include <array>
 #include <climits>
 #include <cstdio>
-#include <esp_log.h>
 
-#pragma once
+#include "esp_log.h"
 
 namespace MapLooper {
 
--- include/MapLooper/Track.hpp
+++ include/MapLooper/Track.hpp
@@ -1,16 +1,19 @@
 /*
-           __
-  /\/\    / /
- /    \  / /
-/ /\/\ \/ /___
-\/    \/\____/
-MapLooper
-(c) Mathias Bredholt 2020
-
-*-0-*-0-*-0-*-0-*-0-*-0-*-0-*-0-*-0-*
-
-Track.hpp
-Class to represent Euclidean tracks with groove system
+ MapLooper - Embedded Live-Looping Tools for Digital Musical Instruments
+ Copyright (C) 2020 Mathias Bredholt
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 
