7b36af18c1228e386817758e89b4cb18694b01d2
mathiasbredholt <mathiasbredholt@gmail.com>
2020-10-24 10:05:59 +0200

    Removed signals handlers, using mpr_sig_get_value instead

--- include/MapLooper/Loop.hpp
+++ include/MapLooper/Loop.hpp
@@ -28,34 +28,14 @@
 namespace MapLooper {
 class Loop {
  public:
-  mpr_sig sigIn;
-  mpr_sig sigOut;
-  mpr_sig sigRecord;
-  mpr_sig sigDelay;
-  mpr_sig sigMod;
-  mpr_sig sigLocalOut;
-  mpr_sig sigLocalIn;
-  mpr_sig sigMute;
-  mpr_map loopMap;
-  mpr_map outMap = 0;
-  mpr_map inMap = 0;
-
   Loop(const char* name, mpr_dev dev, mpr_type type, int vectorSize)
-      : _name(name), _type(type), _vectorSize(vectorSize) {
-    // Create loop signals
+      : _graph(mpr_obj_get_graph(dev)), _type(type), _vectorSize(vectorSize) {
     char sigName[128];
+    int muteMin = 0, muteMax = 1;
     float sigMin = 0.0f, sigMax = 1.0f;
-    float minDelay = -1000.0f, maxDelay = -1.0f;
-    float defaultDelay = -16.0f;
+    float minDelay = -100.0f, maxDelay = -1.0f;
 
-    std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "input");
-    sigIn = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &sigMin,
-                        &sigMax, 0, _sigInHandler, MPR_SIG_UPDATE);
-    mpr_obj_set_prop(sigIn, MPR_PROP_DATA, 0, 1, MPR_PTR, this, 0);
-
-    std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "output");
-    sigOut = mpr_sig_new(dev, MPR_DIR_OUT, sigName, 1, MPR_FLT, 0, &sigMin,
-                         &sigMax, 0, 0, 0);
+    // Create control signals
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "record");
     sigRecord = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &sigMin,
@@ -64,57 +44,50 @@ class Loop {
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "delay");
     sigDelay = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &minDelay,
                            &maxDelay, 0, 0, 0);
-    mpr_sig_set_value(sigDelay, 0, 1, MPR_FLT, &defaultDelay);
+    mpr_sig_set_value(sigDelay, 0, 1, MPR_FLT, &minDelay);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "modulation");
     sigMod = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &sigMin,
                          &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "mute");
-    sigMute = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_INT32, 0, 0, 0, 0,
-                          _sigMuteHandler, MPR_SIG_UPDATE);
-    mpr_obj_set_prop(sigMute, MPR_PROP_DATA, 0, 1, MPR_PTR, this, 0);
+    sigMute = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_INT32, 0, &muteMin,
+                          &muteMax, 0, 0, 0);
+    mpr_sig_set_value(sigMute, 0, 1, MPR_INT32, &muteMin);
+
+    // Create input and output signals
+
+    std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "input");
+    sigIn = mpr_sig_new(dev, MPR_DIR_IN, sigName, _vectorSize, _type, 0,
+                        &sigMin, &sigMax, 0, 0, 0);
+    mpr_sig_set_value(sigIn, 0, _vectorSize, _type, &sigMin);
+
+    std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "output");
+    sigOut = mpr_sig_new(dev, MPR_DIR_OUT, sigName, _vectorSize, _type, 0,
+                         &sigMin, &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "local/out");
-    sigLocalOut = mpr_sig_new(dev, MPR_DIR_OUT, sigName, 1, MPR_FLT, 0, &sigMin,
-                              &sigMax, 0, 0, 0);
+    sigLocalOut = mpr_sig_new(dev, MPR_DIR_OUT, sigName, _vectorSize, _type, 0,
+                              &sigMin, &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "local/in");
-    sigLocalIn = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &sigMin,
-                             &sigMax, 0, _sigLocalInHandler, MPR_SIG_UPDATE);
-    mpr_obj_set_prop(sigLocalIn, MPR_PROP_DATA, 0, 1, MPR_PTR, this, 0);
+    sigLocalIn = mpr_sig_new(dev, MPR_DIR_IN, sigName, _vectorSize, _type, 0,
+                             &sigMin, &sigMax, 0, 0, 0);
 
-    _graph = mpr_obj_get_graph(dev);
-
-    switch (_type) {
-      case MPR_INT32:
-        _frameSize = sizeof(int) * _vectorSize;
-        break;
-      case MPR_FLT:
-        _frameSize = sizeof(float) * _vectorSize;
-        break;
-      default:
-        _frameSize = sizeof(double) * _vectorSize;
-        break;
-    }
-
-    buffer = std::malloc(_frameSize);
-    output = std::malloc(_frameSize);
-    std::memset(buffer, 0, _frameSize);
-    std::memset(output, 0, _frameSize);
+    // Create map
 
     loopMap = mpr_map_new_from_str(
-        "%y=(_%x*%x+(1-_%x)*y{_%x,1000})*((1-_%x)+_%x*uniform(2.0))", sigLocalIn,
+        "%y=(_%x*%x+(1-_%x)*y{_%x,100})*((1-_%x)+_%x*uniform(2.0))", sigLocalIn,
         sigRecord, sigLocalOut, sigRecord, sigDelay, sigMod, sigMod);
     mpr_obj_push(loopMap);
   }
 
   ~Loop() {
-    free(buffer);
-    free(output);
+    mpr_sig_free(sigRecord);
+    mpr_sig_free(sigDelay);
+    mpr_sig_free(sigMod);
     mpr_sig_free(sigIn);
     mpr_sig_free(sigOut);
-    mpr_sig_free(sigRecord);
     mpr_sig_free(sigLocalOut);
     mpr_sig_free(sigLocalIn);
   }
@@ -141,9 +114,18 @@ class Loop {
         printf("Missed %d ticks!\n", now - _lastUpdate - 1);
       }
 
-      // Update buffer and output
-      mpr_sig_set_value(sigLocalOut, 0, _vectorSize, _type, buffer);
-      mpr_sig_set_value(sigOut, 0, _vectorSize, _type, output);
+      // Update local out
+      const void* inputValue = mpr_sig_get_value(sigIn, 0, 0);
+      mpr_sig_set_value(sigLocalOut, 0, _vectorSize, _type, inputValue);
+
+      // Check if muted
+      bool muted = *((int*)mpr_sig_get_value(sigMute, 0, 0));
+
+      if (!muted) {
+        // Update output
+        const void* outputValue = mpr_sig_get_value(sigLocalIn, 0, 0);
+        mpr_sig_set_value(sigOut, 0, _vectorSize, _type, outputValue);
+      }
 
       _lastUpdate = now;
     }
@@ -151,32 +133,19 @@ class Loop {
 
   void setPulsesPerQuarterNote(int value) { _ppqn = value; }
 
+  mpr_sig sigRecord;
+  mpr_sig sigDelay;
+  mpr_sig sigMod;
+  mpr_sig sigIn;
+  mpr_sig sigOut;
+  mpr_sig sigLocalOut;
+  mpr_sig sigLocalIn;
+  mpr_sig sigMute;
+  mpr_map loopMap;
+  mpr_map outMap = 0;
+  mpr_map inMap = 0;
+
  private:
-  static void _sigInHandler(mpr_sig sig, mpr_sig_evt evt, mpr_id inst,
-                            int length, mpr_type type, const void* value,
-                            mpr_time time) {
-    Loop* loop = reinterpret_cast<Loop*>(
-        const_cast<void*>(mpr_obj_get_prop_as_ptr(sig, MPR_PROP_DATA, 0)));
-    std::memcpy(loop->buffer, value, loop->_frameSize);
-  }
-
-  static void _sigLocalInHandler(mpr_sig sig, mpr_sig_evt evt, mpr_id inst,
-                                 int length, mpr_type type, const void* value,
-                                 mpr_time time) {
-    Loop* loop = reinterpret_cast<Loop*>(
-        const_cast<void*>(mpr_obj_get_prop_as_ptr(sig, MPR_PROP_DATA, 0)));
-    std::memcpy(loop->output, value, loop->_frameSize);
-  }
-
-  static void _sigMuteHandler(mpr_sig sig, mpr_sig_evt evt, mpr_id inst,
-                              int length, mpr_type type, const void* value,
-                              mpr_time time) {
-    Loop* loop = const_cast<Loop*>(reinterpret_cast<const Loop*>(
-        mpr_obj_get_prop_as_ptr(sig, MPR_PROP_DATA, 0)));
-    const bool val = *reinterpret_cast<const int*>(value);
-    mpr_obj_set_prop(loop->outMap, MPR_PROP_MUTED, 0, 1, MPR_BOOL, &val, 1);
-  }
-
   void _mapFrom(const char* src, mpr_sig* dst) {
     struct MapData {
       const char* src;
@@ -215,15 +184,10 @@ class Loop {
 
   mpr_graph _graph;
 
-  int _ppqn = 4;
+  int _ppqn = 16;
   int _lastUpdate = 0;
 
-  void* buffer;
-  void* output;
-
-  const char* _name;
   mpr_type _type;
-  size_t _frameSize;
   int _vectorSize;
 };
 }  // namespace MapLooper
