54d1760a75d656802331879a2a63cb8d11ba14bd
mathiasbredholt <mathiasbredholt@gmail.com>
2020-11-06 15:06:58 +0100

    Reverted auto-mapping

--- include/MapLooper/Loop.hpp
+++ include/MapLooper/Loop.hpp
@@ -40,45 +40,46 @@ class Loop {
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "record");
     _sigRecord = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &sigMin,
-                            &sigMax, 0, 0, 0);
+                             &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "delay");
     _sigDelay = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &minDelay,
-                           &maxDelay, 0, 0, 0);
+                            &maxDelay, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "modulation");
     _sigMod = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_FLT, 0, &sigMin,
-                         &sigMax, 0, 0, 0);
+                          &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "mute");
     _sigMute = mpr_sig_new(dev, MPR_DIR_IN, sigName, 1, MPR_INT32, 0, &muteMin,
-                          &muteMax, 0, 0, 0);
+                           &muteMax, 0, 0, 0);
     mpr_sig_set_value(_sigMute, 0, 1, MPR_INT32, &muteMin);
 
     // Create input and output signals
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "input");
     _sigIn = mpr_sig_new(dev, MPR_DIR_IN, sigName, _vectorSize, _type, 0,
-                        &sigMin, &sigMax, 0, 0, 0);
+                         &sigMin, &sigMax, 0, 0, 0);
     mpr_sig_set_value(_sigIn, 0, _vectorSize, _type, &sigMin);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "output");
     _sigOut = mpr_sig_new(dev, MPR_DIR_OUT, sigName, _vectorSize, _type, 0,
-                         &sigMin, &sigMax, 0, 0, 0);
+                          &sigMin, &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "local/out");
     _sigLocalOut = mpr_sig_new(dev, MPR_DIR_OUT, sigName, _vectorSize, _type, 0,
-                              &sigMin, &sigMax, 0, 0, 0);
+                               &sigMin, &sigMax, 0, 0, 0);
 
     std::snprintf(sigName, sizeof(sigName), "%s/%s", name, "local/in");
     _sigLocalIn = mpr_sig_new(dev, MPR_DIR_IN, sigName, _vectorSize, _type, 0,
-                             &sigMin, &sigMax, 0, 0, 0);
+                              &sigMin, &sigMax, 0, 0, 0);
 
     // Create map
 
     _loopMap = mpr_map_new_from_str(
-        "%y=(_%x*%x+(1-_%x)*y{_%x,100})*((1-_%x)+_%x*uniform(2.0))", _sigLocalIn,
-        _sigRecord, _sigLocalOut, _sigRecord, _sigDelay, _sigMod, _sigMod);
+        "%y=(_%x*%x+(1-_%x)*y{_%x,100})*((1-_%x)+_%x*uniform(2.0))",
+        _sigLocalIn, _sigRecord, _sigLocalOut, _sigRecord, _sigDelay, _sigMod,
+        _sigMod);
     mpr_obj_push(_loopMap);
 
     while (!mpr_map_get_is_ready(_loopMap)) {
@@ -98,30 +99,15 @@ class Loop {
     mpr_sig_free(_sigLocalIn);
   }
 
-  template <typename Signal>
-  void mapRecord(Signal src) {
-    _mapFrom(src, &_sigRecord);
-  }
+  void mapRecord(const char* src) { _mapFrom(src, &_sigRecord); }
 
-  template <typename Signal>
-  void mapDelay(Signal src) {
-    _mapFrom(src, &_sigDelay);
-  }
+  void mapDelay(const char* src) { _mapFrom(src, &_sigDelay); }
 
-  template <typename Signal>
-  void mapModulation(Signal src) {
-    _mapFrom(src, &_sigMod);
-  }
+  void mapModulation(const char* src) { _mapFrom(src, &_sigMod); }
 
-  template <typename Signal>
-  void mapInput(Signal src) {
-    _mapFrom(src, &_sigIn);
-  }
+  void mapInput(const char* src) { _mapFrom(src, &_sigIn); }
 
-  template <typename Signal>
-  void mapOutput(Signal dst) {
-    _mapTo(&_sigOut, dst);
-  }
+  void mapOutput(const char* dst) { _mapTo(&_sigOut, dst); }
 
   void update(double beats) {
     int now = beats * _ppqn;
@@ -157,6 +143,8 @@ class Loop {
     setLength(_length);
   }
 
+  int getNumSamples() { return _ppqn * _length; }
+
   float getLength() { return _length; }
 
   void setLength(float beats) {
@@ -165,89 +153,53 @@ class Loop {
     mpr_sig_set_value(_sigDelay, 0, 1, MPR_FLT, &delay);
   }
 
-  mpr_sig getInputSignal() {
-    return _sigIn;
-  }
+  mpr_sig getInputSignal() { return _sigIn; }
 
-  mpr_sig getOutputSignal() {
-    return _sigOut;
-  }
+  mpr_sig getOutputSignal() { return _sigOut; }
 
-  mpr_sig getModulationSignal() {
-    return _sigMod;
-  }
+  mpr_sig getModulationSignal() { return _sigMod; }
 
-  mpr_sig getDelaySignal() {
-    return _sigDelay;
-  }
+  mpr_sig getDelaySignal() { return _sigDelay; }
 
-  mpr_sig getRecordSignal() {
-    return _sigRecord;
-  }
+  mpr_sig getRecordSignal() { return _sigRecord; }
 
-  mpr_sig getMuteSignal() {
-    return _sigMute;
-  }
+  mpr_sig getMuteSignal() { return _sigMute; }
 
  private:
-  template <typename Signal>
-  void _mapFrom(Signal src, mpr_sig* dst) {
-    // Check if type is string or mpr_sig
-    static_assert(std::is_same<Signal, const char*>::value ||
-                      std::is_same<Signal, mpr_sig*>::value,
-                  "Type not mappable");
-    if (std::is_same<Signal, const char*>::value) {
-      // If string, map when signal is found
-      struct MapData {
-        const char* src;
-        mpr_sig* dst;
-      };
-      MapData* mapData = new MapData{src, dst};
+  void _mapFrom(const char* src, mpr_sig* dst) {
+    struct MapData {
+      const char* src;
+      mpr_sig* dst;
+    };
+    MapData* mapData = new MapData{src, dst};
 
-      mpr_graph_handler* handler = [](mpr_graph g, mpr_obj obj,
-                                      const mpr_graph_evt evt,
-                                      const void* data) {
-        MapData* mapData = reinterpret_cast<MapData*>(const_cast<void*>(data));
-        const char* found = mpr_obj_get_prop_as_str(obj, MPR_PROP_NAME, 0);
-        if (strcmp(mapData->src, found) == 0) {
-          mpr_obj_push(mpr_map_new(1, (mpr_sig*)&obj, 1, mapData->dst));
-        }
-      };
-      mpr_graph_add_cb(_graph, handler, MPR_SIG, mapData);
-    } else if (std::is_same<Signal, mpr_sig*>::value) {
-      // If mpr_sig, create map now
-      mpr_obj_push(mpr_map_new(1, src, 1, dst));
-    }
+    mpr_graph_handler* handler = [](mpr_graph g, mpr_obj obj,
+                                    const mpr_graph_evt evt, const void* data) {
+      MapData* mapData = reinterpret_cast<MapData*>(const_cast<void*>(data));
+      const char* found = mpr_obj_get_prop_as_str(obj, MPR_PROP_NAME, 0);
+      if (strcmp(mapData->src, found) == 0) {
+        mpr_obj_push(mpr_map_new(1, (mpr_sig*)&obj, 1, mapData->dst));
+      }
+    };
+    mpr_graph_add_cb(_graph, handler, MPR_SIG, mapData);
   }
 
-  template <typename Signal>
-  void _mapTo(mpr_sig* src, Signal dst) {
-    // Check if type is string or mpr_sig
-    static_assert(std::is_same<Signal, const char*>::value ||
-                      std::is_same<Signal, mpr_sig*>::value,
-                  "Type not mappable");
-    if (std::is_same<Signal, const char*>::value) {
-      // If string, map when signal is found
-      struct MapData {
-        mpr_sig* src;
-        const char* dst;
-      };
-      MapData* mapData = new MapData{src, dst};
+  void _mapTo(mpr_sig* src, const char* dst) {
+    struct MapData {
+      mpr_sig* src;
+      const char* dst;
+    };
+    MapData* mapData = new MapData{src, dst};
 
-      mpr_graph_handler* handler = [](mpr_graph g, mpr_obj obj,
-                                      const mpr_graph_evt evt,
-                                      const void* data) {
-        MapData* mapData = reinterpret_cast<MapData*>(const_cast<void*>(data));
-        const char* found = mpr_obj_get_prop_as_str(obj, MPR_PROP_NAME, 0);
-        if (strcmp(mapData->dst, found) == 0) {
-          mpr_obj_push(mpr_map_new(1, mapData->src, 1, (mpr_sig*)&obj));
-        }
-      };
-      mpr_graph_add_cb(_graph, handler, MPR_SIG, mapData);
-    } else if (std::is_same<Signal, mpr_sig*>::value) {
-      // If mpr_sig, create map now
-      mpr_obj_push(mpr_map_new(1, src, 1, dst));
-    }
+    mpr_graph_handler* handler = [](mpr_graph g, mpr_obj obj,
+                                    const mpr_graph_evt evt, const void* data) {
+      MapData* mapData = reinterpret_cast<MapData*>(const_cast<void*>(data));
+      const char* found = mpr_obj_get_prop_as_str(obj, MPR_PROP_NAME, 0);
+      if (strcmp(mapData->dst, found) == 0) {
+        mpr_obj_push(mpr_map_new(1, mapData->src, 1, (mpr_sig*)&obj));
+      }
+    };
+    mpr_graph_add_cb(_graph, handler, MPR_SIG, mapData);
   }
 
   mpr_graph _graph;
